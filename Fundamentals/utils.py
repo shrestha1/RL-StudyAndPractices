63474ef59ad18720a1558ab22abdb0900013d3cd
dipesh.shrestha <dipesh.shrestha@ekbana.info>
2022-03-28 14:46:50 +0545

    game coordinate to cartesian coordinate and vice versa

---
+++ Fundamentals/utils.py
@@ -0,0 +1,24 @@
+def g2c(coordinate:tuple, pixel_unit:int) -> tuple:
+    """
+        Convert pygame coordinate to cartesian coordinate. x pixel unit 
+        refers to 1 unit in cartesian coordinate system. For example:
+        if pixel unit is 100 then 100 pixel is one unit in cartesian system
+    
+
+    coordinate: game coordinate (x, y)
+    pixel_unit: int
+
+
+    return: tuple
+    """
+    return (int(x/pixel_unit), int(y/pixel_unit))
+
+def c2g(coordinate:tuple, pixel_unit:int) -> tuple:
+    """
+    
+    coordinate: cartesian coordinate (x, y)
+    pixel_unit: int
+
+    return: tuple
+    """
+    return (int(x*pixel_unit), int(y*pixel_unit))
