532aa733cd7b67fe4a2bf8a7c4f87309339ba77f
dipesh.shrestha <dipesh.shrestha@ekbana.info>
2022-03-29 16:48:48 +0545

    changed imports and added screen as attribute

--- Fundamentals/GridEnv/gridenv.py
+++ Fundamentals/GridEnv/gridenv.py
@@ -1,12 +1,5 @@
-import sys
 import pygame
-import time
-
-BLACK = (0, 0, 0)
-WHITE = (200, 200, 200)
-BLUE = (0, 0, 200)
-RED = (200, 0, 0)
-GREEN = (0,125,0)
+from utils import BLACK, GREEN, WHITE, RED
 
 class GridEnv:
     def __init__(self,window_size:tuple, block_size:int):
@@ -15,11 +8,13 @@ class GridEnv:
         self.walls:list
         self.goal:tuple
 
+        self.SCREEN = pygame.display.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
+
     def draw_grid(self):
         for x in range(0, self.WINDOW_WIDTH, self.block_size):
             for y in range(0, self.WINDOW_HEIGHT, self.block_size):
                 rect = pygame.Rect(x,y, self.block_size, self.block_size)
-                pygame.draw.rect(SCREEN, WHITE, rect, 1)
+                pygame.draw.rect(self.SCREEN, WHITE, rect, 1)
 
     def get_walls(self):
         return self.walls
@@ -38,12 +33,12 @@ class GridEnv:
     def draw_walls(self):
         for wall in self.walls:    
             rect = pygame.Rect(wall[0]*self.block_size,wall[1]*self.block_size, self.block_size, self.block_size)
-            pygame.draw.rect(SCREEN, RED, rect)
+            pygame.draw.rect(self.SCREEN, RED, rect)
     
     def draw_goal(self):
         
         rect = pygame.Rect(self.goal[0]*self.block_size,self.goal[1]*self.block_size, self.block_size, self.block_size)
-        pygame.draw.rect(SCREEN, GREEN, rect)
+        pygame.draw.rect(self.SCREEN, GREEN, rect)
 
     def draw(self):
         self.draw_grid()
@@ -53,7 +48,7 @@ class GridEnv:
 
 
 def main():
-    global SCREEN, CLOCK
+    # global SCREEN, CLOCK
     pygame.init()
     SCREEN = pygame.display.set_mode((400, 400))
     CLOCK = pygame.time.Clock()
