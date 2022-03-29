1f157ac8c1be7172e0b82a119c0e669ccf0fd56a
dipesh.shrestha <dipesh.shrestha@ekbana.info>
2022-03-29 16:47:03 +0545

    multiple inheritance and run method

--- Fundamentals/FrozenLake/fl.py
+++ Fundamentals/FrozenLake/fl.py
@@ -1,18 +1,39 @@
+import sys
+import pygame
+import time
+
 from MDP.mdp import MDP
 from GridEnv.gridenv import GridEnv
+
+from utils import BLACK
+
+
 class FrozenLake(MDP, GridEnv):
     """
         A pygame environment for frozenlake is built using the characteristic
         of mdp.
 
     """
-    def __init__(self):
-        self.states = states
-        self.rewards = rewards
-        self.transitions = transitions
-        self.actions = actions
+    def __init__(self, states, actions, rewards, transitions, window_size, block_size):
+        MDP.__init__(self, states, actions, rewards, transitions)
+        GridEnv.__init__(self, window_size, block_size)
 
-if __name__ == "__main__":
-    fl = FrozenLake()
-    
+
+
+    def run(self):
+        # global SCREEN, CLOCK
+        pygame.init()
+        CLOCK = pygame.time.Clock()
+        while True:
+            # print(state, action)
+            self.SCREEN.fill(BLACK)
+            self.draw()
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    print("Exiting the game")
+                    sys.exit()
+            time.sleep(1)
+            pygame.display.update()
 
