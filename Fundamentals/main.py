09c8083f194269d85af5486d1b1c2c40a3421377
dipesh.shrestha <dipesh.shrestha@ekbana.info>
2022-03-29 16:50:02 +0545

    running the main file

--- Fundamentals/main.py
+++ Fundamentals/main.py
@@ -1,3 +1,14 @@
 from FrozenLake.fl import FrozenLake
+from utils import actions, transitions, build_actions, rewards, states, walls_coordinate, Goal_coordinate
 
-fl = FrozenLake()
+WINDOW_COORDINATE = (400,400)
+BLOCK_SIZE = 100
+
+states = states(WINDOW_COORDINATE, BLOCK_SIZE)
+transitions = transitions(states, actions)
+rewards = rewards(states)
+
+fl = FrozenLake(states, actions, rewards, transitions, WINDOW_COORDINATE,BLOCK_SIZE)
+fl.set_goal(Goal_coordinate)
+fl.set_walls(walls_coordinate)
+fl.run()
