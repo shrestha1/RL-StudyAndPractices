a07bd3c65a77f47fb77476a9425fba53a6719663
dipesh.shrestha <dipesh.shrestha@ekbana.info>
2022-03-30 14:53:32 +0545

    added step method

--- Fundamentals/FrozenLake/fl.py
+++ Fundamentals/FrozenLake/fl.py
@@ -17,17 +17,47 @@ class FrozenLake(MDP, GridEnv):
     def __init__(self, states, actions, rewards, transitions, window_size, block_size):
         MDP.__init__(self, states, actions, rewards, transitions)
         GridEnv.__init__(self, window_size, block_size)
+        self.x, self.y = 0, 0
+        self.dx, self.dy = 0, 0
+        self.rect = pygame.rect.Rect((self.x*self.block_size, self.y*self.block_size, self.block_size, self.block_size))
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (self.x, self.y, 128), self.rect)
 
 
+    def step(self,action):
+        
+        if action =="LEFT":
+            self.dx, self.dy =  0, -1
+            time.sleep(1)
+        if action == "RIGHT":
+            self.dx, self.dy = 0, 1
+            time.sleep(1)
+        if action == "UP":
+            self.dx, self.dy = -1, 0
+            time.sleep(1)
+        if action =="DOWN":
+            self.dx, self.dy = 1, 0
+            time.sleep(1)
+        
+        self.x += self.dx
+        self.y += self.dy
 
-    def run(self):
+        self.rect = pygame.rect.Rect((self.x*self.block_size, self.y*self.block_size, self.block_size, self.block_size))
+        self.dx, self.dy = 0, 0
+
+
+    def run(self, policy):
         # global SCREEN, CLOCK
         pygame.init()
         CLOCK = pygame.time.Clock()
         while True:
             # print(state, action)
             self.SCREEN.fill(BLACK)
-            self.draw()
+            self.draw_env()
+            self.draw(self.SCREEN)
+            
+            self.step(policy[(self.x,self.y)])
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
