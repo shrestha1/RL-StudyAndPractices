6a902048a2fd2ffabe69e5a8ef940f3f51f56bd5
dipesh.shrestha <dipesh.shrestha@ekbana.info>
2022-03-30 14:54:42 +0545

    updated main.py

--- Fundamentals/main.py
+++ Fundamentals/main.py
@@ -1,5 +1,6 @@
 from FrozenLake.fl import FrozenLake
 from utils import actions, transitions, build_actions, rewards, states, walls_coordinate, Goal_coordinate
+from algorithms import policy
 
 WINDOW_COORDINATE = (400,400)
 BLOCK_SIZE = 100
@@ -11,4 +12,5 @@ rewards = rewards(states)
 fl = FrozenLake(states, actions, rewards, transitions, WINDOW_COORDINATE,BLOCK_SIZE)
 fl.set_goal(Goal_coordinate)
 fl.set_walls(walls_coordinate)
-fl.run()
+policy = policy(fl)
+fl.run(policy)
